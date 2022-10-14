fa0ff496f9a776c943be395bbf6393d9bb2b10c8
FabioLongoni <longoni.fabio90@gmail.com>
2022-10-14 16:30:23 +0200

    add show.blade

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('metaTitle', $comic->title)
+
+@section('content')
+
+<div>
+  <div class="hero">
+  </div>
+  <div class="line__blue">
+    <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+  </div>
+  <div class="container__sm">
+    <div class="principal__info">
+      <div class="principal__info__text">
+        <h1>{{$comic->title}}</h1>
+        <div class="info__price">
+          <div>
+            U.S. Price:
+          </div>
+          <div>
+            {{$comic->price}}
+          </div>
+          <div class="av">
+            available
+          </div>
+        </div>
+        <div class="description">
+          {{$comic->description}}
+        </div>
+      </div>
+      <div>
+        <img class="apply" src="/img/adv.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
